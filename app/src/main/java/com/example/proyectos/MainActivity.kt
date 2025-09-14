f6d9558a389e04f29400b1fe31bb3ba8656c091f
Santiagorozco28 <orozcosantiago937@gmail.com>
2025-09-13 21:32:25 -0600

    Practica3 agregue imagen

--- app/src/main/java/com/example/proyectos/MainActivity.kt
+++ app/src/main/java/com/example/proyectos/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -25,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,6 +56,12 @@ fun ISRCalculator() {
     var isr by remember { mutableStateOf("") }
     var sueldoNeto by remember { mutableStateOf("") }
 
+    Row() {
+        Image(
+            painter = painterResource(id = R.drawable.sad),
+            contentDescription = null
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
